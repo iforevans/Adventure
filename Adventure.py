7f04d51cee3f5366c197b540a776b5a13bcaac78
Iforevans <ifor.evans@gmail.com>
2024-10-18 09:21:54 +0200

    Added methods to the location class to get and drop items at that location.

--- Adventure.py
+++ Adventure.py
@@ -1,14 +1,17 @@
 # This module will hold most of our classes
 
 class Location(object):
-
     def __init__(self, loc_id, description):
         self._description = description
         self._loc_id = loc_id
+        self._exits = {}
+        self._items = []
 
+    # Set all the possible exits from this location
     def SetExits(self, N, E, S, W, I, O, U, D):
        self._exits = {"north":N, "east":E, "south":S, "west":W, "in":I, "out":O, "up":U, "down":D}
 
+    # Get the description of the location
     def Describe(self):
         print(f"{self._description}")
         print(f"Possible exits are:", end=" ")
@@ -16,11 +19,22 @@ class Location(object):
             if self._exits[key] != -1:
                 print(f" {key}")
 
+    # Move to a new location (if possible)
     def Move(self, direction):
         if direction.lower() in self._exits:
             return self._exits[direction]
         else:
             return -1
+
+    # Drop an item at this location
+    def DropItem(self, item_id):
+        self._items.append()
+
+    # Get an item from this location
+    def GetItem(self, item_id):
+        if item_id in self._items
+            self._items.remove(item_id)
+    
 
 
 class Item(object):
@@ -29,5 +43,20 @@ class Item(object):
         self._short_description = short_description
         self._long_description = long_description
         self._weight = weight
+
+    def ItemId(self):
+        return self._item_id
+
+    def LongDescription(self):
+        return self._long_description
+
+    def ShortDescription(self):
+        return self._short_description
+
+    def Weight(self):
+        return self._weight
+
+
+
 
 
